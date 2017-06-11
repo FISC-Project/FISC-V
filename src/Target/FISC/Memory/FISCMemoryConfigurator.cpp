a423572626cbc1e251af7f3487774d5356369426
Miguel <santosmiguel25@gmail.com>
2017-06-11 06:58:39 +0100

    Tabs replaced with spaces

--- src/Target/FISC/Memory/FISCMemoryConfigurator.cpp
+++ src/Target/FISC/Memory/FISCMemoryConfigurator.cpp
@@ -1,11 +1,11 @@
 /*-----------------------------------------------------------------------------
-						   __  __          ___                                |
-					  |\/|/  \|  \|  ||   |__ .                               |
-					  |  |\__/|__/\__/|___|___.                               |
-		  ___     __  __        __  __      ___ __      __    _____  __       |
-	 |\/||__ |\/|/  \|__)\ /   /  `/  \|\ ||__|/ _`|  ||__) /\ |/  \|__)      |
-	 |  ||___|  |\__/|  \ |    \__,\__/| \||  |\__>\__/|  \/~~\|\__/|  \      |
-	                                                                          |
+                           __  __          ___                                |
+                      |\/|/  \|  \|  ||   |__ .                               |
+                      |  |\__/|__/\__/|___|___.                               |
+          ___     __  __        __  __      ___ __      __    _____  __       |
+     |\/||__ |\/|/  \|__)\ /   /  `/  \|\ ||__|/ _`|  ||__) /\ |/  \|__)      |
+     |  ||___|  |\__/|  \ |    \__,\__/| \||  |\__>\__/|  \/~~\|\__/|  \      |
+                                                                              |
 -----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------
@@ -29,108 +29,108 @@
 class MemoryConfigurator : public ConfigPass {
 #pragma region REGION 1: THE MEMORY CONFIGURATION DATA
 public:
-	/* Pass properties */
-	#define MEMORY_CONFIGURATOR_PRIORITY 1 /* The execution priority of this module */
-	#define MEMORY_PROGRAM_FILE_IOS_MODE std::ios::in | std::ios::binary /* The mode the program file will be opened with */
+    /* Pass properties */
+    #define MEMORY_CONFIGURATOR_PRIORITY 1 /* The execution priority of this module */
+    #define MEMORY_PROGRAM_FILE_IOS_MODE std::ios::in | std::ios::binary /* The mode the program file will be opened with */
 
-	/* List of permissions for external Passes that want to use the resources of this Pass */
-	#define WHITELIST_MEM_CONFIG {DECL_WHITELIST_ALL(MemoryModule)}
+    /* List of permissions for external Passes that want to use the resources of this Pass */
+    #define WHITELIST_MEM_CONFIG {DECL_WHITELIST_ALL(MemoryModule)}
 
-	/* Command line flags */
-	#define MEMORY_FLAG_BOOT_SHORT 'b'
-	#define MEMORY_FLAG_BOOT_LONG "boot"
+    /* Command line flags */
+    #define MEMORY_FLAG_BOOT_SHORT 'b'
+    #define MEMORY_FLAG_BOOT_LONG "boot"
 
-	/* Implementation properties */
-	#define MEMORY_WIDTH   8    /* The width of the memory */
-	#define MEMORY_DEPTH   1024 /* Size of memory in bytes */
-	#define MEMORY_LOADLOC 0    /* Where to load the program on startup */
+    /* Implementation properties */
+    #define MEMORY_WIDTH   8    /* The width of the memory */
+    #define MEMORY_DEPTH   1024 /* Size of memory in bytes */
+    #define MEMORY_LOADLOC 0    /* Where to load the program on startup */
 #pragma endregion
 
 #pragma region REGION 2: THE MEMORY STRUCTURE DEFINITION (IMPL. SPECIFIC)
 public:
-	std::vector<std::bitset<MEMORY_WIDTH> > theMemory; /* The actual main memory */
-	uint64_t loadedProgramSize; /* Size of the loaded program */
-	File programFile; /* The file being loaded into memory */
-	/* DISCLAIMER: In the future, we might want to care about a singular
-	   file being loaded into memory as the absolute program being loaded. 
-	   In other words, the first program that is loaded might be responsible for
-	   loading other files, thus rendering this variable useless. */
+    std::vector<std::bitset<MEMORY_WIDTH> > theMemory; /* The actual main memory */
+    uint64_t loadedProgramSize; /* Size of the loaded program */
+    File programFile; /* The file being loaded into memory */
+    /* DISCLAIMER: In the future, we might want to care about a singular
+       file being loaded into memory as the absolute program being loaded. 
+       In other words, the first program that is loaded might be responsible for
+       loading other files, thus rendering this variable useless. */
 #pragma endregion
 
 #pragma region REGION 3: THE MEMORY CONFIGURATION IMPLEMENTATION (IMPL SPECIFIC)
 public:
-	uint64_t getMemSize() {
-		return theMemory.size();
-	}
+    uint64_t getMemSize() {
+        return theMemory.size();
+    }
 
-	uint64_t getProgSize() {
-		return loadedProgramSize;
-	}
+    uint64_t getProgSize() {
+        return loadedProgramSize;
+    }
 #pragma endregion
 
 #pragma region REGION 4: THE MEMORY CONFIGURATION IMPLEMENTATION (GENERIC VM FUNCTIONS)
 public:
-	MemoryConfigurator() : ConfigPass(MEMORY_CONFIGURATOR_PRIORITY),
-		loadedProgramSize(0), theMemory(MEMORY_DEPTH, -1), programFile(NULLSTR, 0)
-	{
-		setWhitelist(WHITELIST_MEM_CONFIG);
-	}
-	
-	enum PassRetcode init() {
-		enum PassRetcode success = PASS_RET_ERR;
-		printf("- Initializating Memory\n");
+    MemoryConfigurator() : ConfigPass(MEMORY_CONFIGURATOR_PRIORITY),
+        loadedProgramSize(0), theMemory(MEMORY_DEPTH, -1), programFile(NULLSTR, 0)
+    {
+        setWhitelist(WHITELIST_MEM_CONFIG);
+    }
+    
+    enum PassRetcode init() {
+        enum PassRetcode success = PASS_RET_ERR;
+        printf("- Initializating Memory\n");
 
-		/* Setup program file */
-		if (cmdHasOpt(MEMORY_FLAG_BOOT_SHORT)) {
-			std::pair<char, std::string> fileName = cmdQuery(MEMORY_FLAG_BOOT_SHORT);
-			/* Create file only if it exists */
-			if (programFile.create(fileName.second, MEMORY_PROGRAM_FILE_IOS_MODE))
-				success = PASS_RET_OK;
-		}
-		if (cmdHasOpt(MEMORY_FLAG_BOOT_LONG)) {
-			std::pair<std::string, std::string> fileName = cmdQuery(MEMORY_FLAG_BOOT_LONG);
-			/* Create file only if it exists */
-			if (programFile.create(fileName.second, MEMORY_PROGRAM_FILE_IOS_MODE))
-				success = PASS_RET_OK;
-		}
+        /* Setup program file */
+        if (cmdHasOpt(MEMORY_FLAG_BOOT_SHORT)) {
+            std::pair<char, std::string> fileName = cmdQuery(MEMORY_FLAG_BOOT_SHORT);
+            /* Create file only if it exists */
+            if (programFile.create(fileName.second, MEMORY_PROGRAM_FILE_IOS_MODE))
+                success = PASS_RET_OK;
+        }
+        if (cmdHasOpt(MEMORY_FLAG_BOOT_LONG)) {
+            std::pair<std::string, std::string> fileName = cmdQuery(MEMORY_FLAG_BOOT_LONG);
+            /* Create file only if it exists */
+            if (programFile.create(fileName.second, MEMORY_PROGRAM_FILE_IOS_MODE))
+                success = PASS_RET_OK;
+        }
 
-		if (success == PASS_RET_ERR) {
-			/* TODO: If the variable success is equal to PASS_RET_ERR,
-			we must tell the top layer (VM) that this module
-			will not be able to continue, thus forcing every other
-			single module (on this target only) to cancel its current
-			operations. */
-			printf("\n>> ERROR: Could not initialize the Memory Configurator Pass!\n\n");
-		}
+        if (success == PASS_RET_ERR) {
+            /* TODO: If the variable success is equal to PASS_RET_ERR,
+            we must tell the top layer (VM) that this module
+            will not be able to continue, thus forcing every other
+            single module (on this target only) to cancel its current
+            operations. */
+            printf("\n>> ERROR: Could not initialize the Memory Configurator Pass!\n\n");
+        }
 
-		return success;
-	}
+        return success;
+    }
 
-	enum PassRetcode finit() {
-		/* Nothing to do for now */
-		return PASS_RET_OK;
-	}
+    enum PassRetcode finit() {
+        /* Nothing to do for now */
+        return PASS_RET_OK;
+    }
 
-	enum PassRetcode run() {
-		/* For now we don't have anything to configure.
-		   In the future, we might want to read and parse a config file (using init() function and not run())
-		   and select the desired configurations on the parsed results here.
-		   These config files could contain information about memory size (no need to hardcode them here),
-		   memory access speeds, restrictions, and just about any memory related property. */
-		return PASS_RET_OK;
-	}
+    enum PassRetcode run() {
+        /* For now we don't have anything to configure.
+           In the future, we might want to read and parse a config file (using init() function and not run())
+           and select the desired configurations on the parsed results here.
+           These config files could contain information about memory size (no need to hardcode them here),
+           memory access speeds, restrictions, and just about any memory related property. */
+        return PASS_RET_OK;
+    }
 
-	enum PassRetcode watchdog() {
-		return PASS_RET_OK;
-	}
+    enum PassRetcode watchdog() {
+        return PASS_RET_OK;
+    }
 #pragma endregion
 };
 /*-----------------------------------------------------------------------------
-			  ___     __     __  ___        __  __          ___               |
-			 |__ |\ ||  \   /  \|__    |\/|/  \|  \|  ||   |__ .              |
-			 |___| \||__/   \__/|      |  |\__/|__/\__/|___|___.              |
-		  ___     __  __        __  __      ___ __      __    _____  __       |
-	 |\/||__ |\/|/  \|__)\ /   /  `/  \|\ ||__|/ _`|  ||__) /\ |/  \|__)      |
-	 |  ||___|  |\__/|  \ |    \__,\__/| \||  |\__>\__/|  \/~~\|\__/|  \      |
-																			  |
+              ___     __     __  ___        __  __          ___               |
+             |__ |\ ||  \   /  \|__    |\/|/  \|  \|  ||   |__ .              |
+             |___| \||__/   \__/|      |  |\__/|__/\__/|___|___.              |
+          ___     __  __        __  __      ___ __      __    _____  __       |
+     |\/||__ |\/|/  \|__)\ /   /  `/  \|\ ||__|/ _`|  ||__) /\ |/  \|__)      |
+     |  ||___|  |\__/|  \ |    \__,\__/| \||  |\__>\__/|  \/~~\|\__/|  \      |
+                                                                              |
 -----------------------------------------------------------------------------*/
--- src/Target/FISC/Memory/FISCMemoryModule.cpp
+++ src/Target/FISC/Memory/FISCMemoryModule.cpp
@@ -1,11 +1,11 @@
 /*---------------------------------------------------------------
-					 __  __          ___                        |
-				|\/|/  \|  \|  ||   |__ .                       |
-				|  |\__/|__/\__/|___|___.                       |
-		  ___     __  __            __  __          ___         |
-	 |\/||__ |\/|/  \|__)\ /   |\/|/  \|  \|  ||   |__          |
-	 |  ||___|  |\__/|  \ |    |  |\__/|__/\__/|___|___         |
-																|
+                     __  __          ___                        |
+                |\/|/  \|  \|  ||   |__ .                       |
+                |  |\__/|__/\__/|___|___.                       |
+          ___     __  __            __  __          ___         |
+     |\/||__ |\/|/  \|__)\ /   |\/|/  \|  \|  ||   |__          |
+     |  ||___|  |\__/|  \ |    |  |\__/|__/\__/|___|___         |
+                                                                |
 ---------------------------------------------------------------*/
 
 /*----------------------------------------------------
@@ -27,178 +27,178 @@
 class MemoryModule : public RunPass {
 #pragma region REGION 1: THE MEMORY CONFIGURATION DATA
 private:
-	/* Pass properties */
-	#define MEMORY_MODULE_PRIORITY 1 /* The execution priority of this module */
+    /* Pass properties */
+    #define MEMORY_MODULE_PRIORITY 1 /* The execution priority of this module */
 
-	/* List of permissions for external Passes that want to use the resources of this Pass */
-	#define WHITELIST_MEM_MOD {DECL_WHITELIST_ALL(PipelineModule)}
+    /* List of permissions for external Passes that want to use the resources of this Pass */
+    #define WHITELIST_MEM_MOD {DECL_WHITELIST_ALL(PipelineModule)}
 #pragma endregion
 
 #pragma region REGION 2: THE MEMORY STRUCTURE DEFINITION (IMPL. SPECIFIC)
 private:
-	/* External Pass handles */
-	MemoryConfigurator * mconf;
+    /* External Pass handles */
+    MemoryConfigurator * mconf;
 #pragma endregion
 
 #pragma region REGION 3: THE MEMORY BEHAVIOUR (IMPL. SPECIFIC)
 
 
 public:
-	uint64_t read(uint32_t address, enum FISC_DATATYPE dataType, bool align) {
-		/* Align (or not) the address */
-		if(align)
-			alignAddress(address, dataType);
+    uint64_t read(uint32_t address, enum FISC_DATATYPE dataType, bool align) {
+        /* Align (or not) the address */
+        if(align)
+            alignAddress(address, dataType);
 
-		/* Check if address is valid */
-		if(!isAddressValid(address, dataType))
-			return (uint64_t)-1;
+        /* Check if address is valid */
+        if(!isAddressValid(address, dataType))
+            return (uint64_t)-1;
 
-		/* Fetch the memory */
-		switch (dataType) {
-		case FISC_SZ_8:
-			return mconf->theMemory[address].to_ulong();
-		case FISC_SZ_16:
-			return (mconf->theMemory[address].to_ulong() << 8) |
-				mconf->theMemory[address + 1].to_ulong();
-		case FISC_SZ_32:
-			return  (mconf->theMemory[address].to_ulong() << 24) |
-				(mconf->theMemory[address + 1].to_ulong() << 16) |
-				(mconf->theMemory[address + 2].to_ulong() << 8) |
-				mconf->theMemory[address + 3].to_ulong();
-		case FISC_SZ_64:
-			return ((uint64_t)(mconf->theMemory[address].to_ulong()) << 56) |
-				((uint64_t)mconf->theMemory[address + 1].to_ulong() << 48) |
-				((uint64_t)mconf->theMemory[address + 2].to_ulong() << 40) |
-				((uint64_t)mconf->theMemory[address + 3].to_ulong() << 32) |
-				((uint64_t)mconf->theMemory[address + 4].to_ulong() << 24) |
-				((uint64_t)mconf->theMemory[address + 5].to_ulong() << 16) |
-				((uint64_t)mconf->theMemory[address + 6].to_ulong() << 8) |
-				(uint64_t)mconf->theMemory[address + 7].to_ulong();
-		default: return (uint64_t)-1;
-		}
-		return (uint64_t)-1;
-	}
+        /* Fetch the memory */
+        switch (dataType) {
+        case FISC_SZ_8:
+            return mconf->theMemory[address].to_ulong();
+        case FISC_SZ_16:
+            return (mconf->theMemory[address].to_ulong() << 8) |
+                mconf->theMemory[address + 1].to_ulong();
+        case FISC_SZ_32:
+            return  (mconf->theMemory[address].to_ulong() << 24) |
+                (mconf->theMemory[address + 1].to_ulong() << 16) |
+                (mconf->theMemory[address + 2].to_ulong() << 8) |
+                mconf->theMemory[address + 3].to_ulong();
+        case FISC_SZ_64:
+            return ((uint64_t)(mconf->theMemory[address].to_ulong()) << 56) |
+                ((uint64_t)mconf->theMemory[address + 1].to_ulong() << 48) |
+                ((uint64_t)mconf->theMemory[address + 2].to_ulong() << 40) |
+                ((uint64_t)mconf->theMemory[address + 3].to_ulong() << 32) |
+                ((uint64_t)mconf->theMemory[address + 4].to_ulong() << 24) |
+                ((uint64_t)mconf->theMemory[address + 5].to_ulong() << 16) |
+                ((uint64_t)mconf->theMemory[address + 6].to_ulong() << 8) |
+                (uint64_t)mconf->theMemory[address + 7].to_ulong();
+        default: return (uint64_t)-1;
+        }
+        return (uint64_t)-1;
+    }
 
-	bool write(uint64_t data, uint32_t address, enum FISC_DATATYPE dataType, bool align) {
-		/* Align (or not) the address */
-		if (align)
-			alignAddress(address, dataType);
+    bool write(uint64_t data, uint32_t address, enum FISC_DATATYPE dataType, bool align) {
+        /* Align (or not) the address */
+        if (align)
+            alignAddress(address, dataType);
 
-		/* Check if address is valid */
-		if(!isAddressValid(address, dataType))
-			return false;
+        /* Check if address is valid */
+        if(!isAddressValid(address, dataType))
+            return false;
 
-		/* Write to memory */
-		switch (dataType) {
-		case FISC_SZ_8:
-			mconf->theMemory[address] = (uint8_t)data;
-			break;
-		case FISC_SZ_16:
-			mconf->theMemory[address] = (uint8_t)((data & 0xFF00) >> 8);
-			mconf->theMemory[address + 1] = (uint8_t)data & 0xFF;
-			break;
-		case FISC_SZ_32:
-			mconf->theMemory[address] = (uint8_t)((data & 0xFF000000) >> 24);
-			mconf->theMemory[address + 1] = (uint8_t)((data & 0xFF0000) >> 16);
-			mconf->theMemory[address + 2] = (uint8_t)((data & 0xFF00) >> 8);
-			mconf->theMemory[address + 3] = (uint8_t)data & 0xFF;
-			break;
-		case FISC_SZ_64:
-			mconf->theMemory[address] = (uint8_t)((data & 0xFF00000000000000) >> 56);
-			mconf->theMemory[address + 1] = (uint8_t)((data & 0xFF000000000000) >> 48);
-			mconf->theMemory[address + 2] = (uint8_t)((data & 0xFF0000000000) >> 40);
-			mconf->theMemory[address + 3] = (uint8_t)((data & 0xFF00000000) >> 32);
-			mconf->theMemory[address + 4] = (uint8_t)((data & 0xFF000000) >> 24);
-			mconf->theMemory[address + 5] = (uint8_t)((data & 0xFF0000) >> 16);
-			mconf->theMemory[address + 6] = (uint8_t)((data & 0xFF00) >> 8);
-			mconf->theMemory[address + 7] = (uint8_t)data & 0xFF;
-			break;
-		default: return false;
-		}
-		return true;
-	}
+        /* Write to memory */
+        switch (dataType) {
+        case FISC_SZ_8:
+            mconf->theMemory[address] = (uint8_t)data;
+            break;
+        case FISC_SZ_16:
+            mconf->theMemory[address] = (uint8_t)((data & 0xFF00) >> 8);
+            mconf->theMemory[address + 1] = (uint8_t)data & 0xFF;
+            break;
+        case FISC_SZ_32:
+            mconf->theMemory[address] = (uint8_t)((data & 0xFF000000) >> 24);
+            mconf->theMemory[address + 1] = (uint8_t)((data & 0xFF0000) >> 16);
+            mconf->theMemory[address + 2] = (uint8_t)((data & 0xFF00) >> 8);
+            mconf->theMemory[address + 3] = (uint8_t)data & 0xFF;
+            break;
+        case FISC_SZ_64:
+            mconf->theMemory[address] = (uint8_t)((data & 0xFF00000000000000) >> 56);
+            mconf->theMemory[address + 1] = (uint8_t)((data & 0xFF000000000000) >> 48);
+            mconf->theMemory[address + 2] = (uint8_t)((data & 0xFF0000000000) >> 40);
+            mconf->theMemory[address + 3] = (uint8_t)((data & 0xFF00000000) >> 32);
+            mconf->theMemory[address + 4] = (uint8_t)((data & 0xFF000000) >> 24);
+            mconf->theMemory[address + 5] = (uint8_t)((data & 0xFF0000) >> 16);
+            mconf->theMemory[address + 6] = (uint8_t)((data & 0xFF00) >> 8);
+            mconf->theMemory[address + 7] = (uint8_t)data & 0xFF;
+            break;
+        default: return false;
+        }
+        return true;
+    }
 
 private:
-	uint32_t alignAddress(uint32_t & address, enum FISC_DATATYPE dataType) {
-		switch (dataType) {
-			case FISC_SZ_8: /* No need to align */       break;
-			case FISC_SZ_16: address = ALIGN16(address); break;
-			case FISC_SZ_32: address = ALIGN32(address); break;
-			case FISC_SZ_64: address = ALIGN64(address); break;
-			default: return (uint32_t)-1;
-		}
-		return address;
-	}
+    uint32_t alignAddress(uint32_t & address, enum FISC_DATATYPE dataType) {
+        switch (dataType) {
+            case FISC_SZ_8: /* No need to align */       break;
+            case FISC_SZ_16: address = ALIGN16(address); break;
+            case FISC_SZ_32: address = ALIGN32(address); break;
+            case FISC_SZ_64: address = ALIGN64(address); break;
+            default: return (uint32_t)-1;
+        }
+        return address;
+    }
 
-	bool isAddressValid(uint32_t address, enum FISC_DATATYPE dataType) {
-		switch (dataType) {
-			case FISC_SZ_8: /* Intentional fallthrough */
-			case FISC_SZ_16: 
-			case FISC_SZ_32:
-			case FISC_SZ_64: return address < mconf->theMemory.size();
-			default: return false;
-		}
-	}
+    bool isAddressValid(uint32_t address, enum FISC_DATATYPE dataType) {
+        switch (dataType) {
+            case FISC_SZ_8: /* Intentional fallthrough */
+            case FISC_SZ_16: 
+            case FISC_SZ_32:
+            case FISC_SZ_64: return address < mconf->theMemory.size();
+            default: return false;
+        }
+    }
 #pragma endregion
 
 #pragma region REGION 4: THE MEMORY BEHAVIOUR (GENERIC VM FUNCTIONS)
 public:
-	MemoryModule() : RunPass(MEMORY_MODULE_PRIORITY)
-	{
-		setWhitelist(WHITELIST_MEM_MOD);
-	}
+    MemoryModule() : RunPass(MEMORY_MODULE_PRIORITY)
+    {
+        setWhitelist(WHITELIST_MEM_MOD);
+    }
 
-	enum PassRetcode init() {
-		enum PassRetcode success = PASS_RET_OK;
-		/* Fetch Memory Configurator Pass */
-		if (!(mconf = GET_PASS(MemoryConfigurator))) {
-			/* TODO: We were unable to find a MemoryConfigurator pass!
-			   We cannot continue the execution of this pass */
-			printf("\n>> ERROR: Could not fetch the Memory Configurator Pass!\n\n");
-			success = PASS_RET_ERR;
-		}
+    enum PassRetcode init() {
+        enum PassRetcode success = PASS_RET_OK;
+        /* Fetch Memory Configurator Pass */
+        if (!(mconf = GET_PASS(MemoryConfigurator))) {
+            /* TODO: We were unable to find a MemoryConfigurator pass!
+               We cannot continue the execution of this pass */
+            printf("\n>> ERROR: Could not fetch the Memory Configurator Pass!\n\n");
+            success = PASS_RET_ERR;
+        }
 
-		if (success == PASS_RET_OK) {
-			/* Load up the memory */
-			mconf->programFile.open();
-			std::string tmpstr;
-			for (unsigned int i = MEMORY_LOADLOC; i < mconf->getMemSize() && i < mconf->programFile.fileSize(); i++) {
-				tmpstr = mconf->programFile.read(MEMORY_WIDTH / 8);
-				if (tmpstr.size() == 1) {
-					mconf->theMemory[i] = std::bitset<MEMORY_WIDTH>(tmpstr[0]);
-					mconf->loadedProgramSize++;
-				} else {
-					break; /* We've reached EOF */
-				}
-			}
-			mconf->programFile.close();
-			printf("- Loaded %d bytes into memory\n", (unsigned int)mconf->loadedProgramSize);
-		}
-		return success;
-	}
-	
-	enum PassRetcode finit() {
-		printf("- Terminating Memory\n");
-		return PASS_RET_OK;
-	}
+        if (success == PASS_RET_OK) {
+            /* Load up the memory */
+            mconf->programFile.open();
+            std::string tmpstr;
+            for (unsigned int i = MEMORY_LOADLOC; i < mconf->getMemSize() && i < mconf->programFile.fileSize(); i++) {
+                tmpstr = mconf->programFile.read(MEMORY_WIDTH / 8);
+                if (tmpstr.size() == 1) {
+                    mconf->theMemory[i] = std::bitset<MEMORY_WIDTH>(tmpstr[0]);
+                    mconf->loadedProgramSize++;
+                } else {
+                    break; /* We've reached EOF */
+                }
+            }
+            mconf->programFile.close();
+            printf("- Loaded %d bytes into memory\n", (unsigned int)mconf->loadedProgramSize);
+        }
+        return success;
+    }
+    
+    enum PassRetcode finit() {
+        printf("- Terminating Memory\n");
+        return PASS_RET_OK;
+    }
 
-	enum PassRetcode run() {
-		/* TODO: For now we don't want to keep anything running on this thread.
-		   We're keeping it relatively simple (for now!!) */
-		return PASS_RET_OK;
-	}
+    enum PassRetcode run() {
+        /* TODO: For now we don't want to keep anything running on this thread.
+           We're keeping it relatively simple (for now!!) */
+        return PASS_RET_OK;
+    }
 
-	enum PassRetcode watchdog() {
-		return PASS_RET_OK;
-	}
+    enum PassRetcode watchdog() {
+        return PASS_RET_OK;
+    }
 #pragma endregion
 };
 /*---------------------------------------------------------------
-	 ___     __     __  ___        __  __          ___          |
-	|__ |\ ||  \   /  \|__    |\/|/  \|  \|  ||   |__ .         |
-	|___| \||__/   \__/|      |  |\__/|__/\__/|___|___.         |
-		  ___     __  __            __  __          ___         |
-	 |\/||__ |\/|/  \|__)\ /   |\/|/  \|  \|  ||   |__          |
-	 |  ||___|  |\__/|  \ |    |  |\__/|__/\__/|___|___         |
-																|
+     ___     __     __  ___        __  __          ___          |
+    |__ |\ ||  \   /  \|__    |\/|/  \|  \|  ||   |__ .         |
+    |___| \||__/   \__/|      |  |\__/|__/\__/|___|___.         |
+          ___     __  __            __  __          ___         |
+     |\/||__ |\/|/  \|__)\ /   |\/|/  \|  \|  ||   |__          |
+     |  ||___|  |\__/|  \ |    |  |\__/|__/\__/|___|___         |
+                                                                |
 ----------------------------------------------------------------*/
